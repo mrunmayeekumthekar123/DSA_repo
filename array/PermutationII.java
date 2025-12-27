a8e0b050fd56215e9c4ceff16cb0446dc4af4c67
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2025-12-27 17:28:35 +0530

    Update PermutationII.java

--- array/PermutationII.java
+++ array/PermutationII.java
@@ -1,1 +1,27 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
 
+    private void backtrack(List<List<Integer>> res, List<Integer> templist, int[] nums, boolean[] used){
+        if(templist.size()==nums.length){
+            res.add(new ArrayList<>(templist));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+
+            used[i]=true;
+            templist.add(nums[i]);
+            backtrack(res, templist, nums, used);
+
+            used[i]=false;
+            templist.remove(templist.size()-1);
+        }
+    }
+}
