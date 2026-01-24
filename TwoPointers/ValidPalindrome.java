14b44a1ba4624af5bfdd978cd095d00a647a80fe
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2026-01-24 16:41:06 +0530

    Update ValidPalindrome.java

--- TwoPointers/ValidPalindrome.java
+++ TwoPointers/ValidPalindrome.java
@@ -1,1 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<right){
+            char LeftChar = s.charAt(left);
+            char RightChar = s.charAt(right);
+
+
+            if(!Character.isLetterOrDigit(LeftChar)){
+                left++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(RightChar)){
+                right--;
+                continue;
+            }
+
+            if(Character.toLowerCase(LeftChar)!=Character.toLowerCase(RightChar)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
 
