e7bc6395d23eec4d241710a0b1468d9478c50747
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2026-01-29 14:51:54 +0530

    Update graycode.java

--- math/graycode.java
+++ math/graycode.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int x = 1 << n; 
+        List<Integer> list = new ArrayList<>(x);
+        for (int i = 0; i < x; i++) {
+            list.add(i ^ (i >> 1));
+        }
+        return list;
+    }
+}
