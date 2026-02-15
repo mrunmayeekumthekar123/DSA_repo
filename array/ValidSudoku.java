fdc6bc6567c27d40f92575df33345f2d45b09bb8
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2026-02-15 17:42:24 +0530

    Update ValidSudoku.java

--- array/ValidSudoku.java
+++ array/ValidSudoku.java
@@ -1,1 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0; i<9;i++){
+            for(int j=0; j<9; j++){
+                if(!isValid(i,j,board)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int row, int col, char[][] board){
+       for(int i=0; i<9;i++){
+        if(board[row][col]=='.') continue;
+        if(col!=i && board[row][i]==board[row][col])return false;
+        if(row!=i && board[i][col]==board[row][col]) return false;
+        if(row!=(3*(row/3)+i/3) && col !=(3*(col/3)+(i%3)) && board[3*(row/3)+i/3][3*(col/3)+(i%3)]==board[row][col]) return false;
+       }
+       return true;
+    }
+}
 
