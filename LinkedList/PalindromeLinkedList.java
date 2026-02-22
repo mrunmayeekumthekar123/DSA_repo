4791bdfcb953c373fc4844b3cb77f77c1123720e
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2026-02-22 17:51:39 +0530

    Update PalindromeLinkedList.java

--- LinkedList/PalindromeLinkedList.java
+++ LinkedList/PalindromeLinkedList.java
@@ -1,1 +1,36 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        int n=0;
+        ListNode temp=head;
+        while(temp!=null){
+            n++;
+            temp=temp.next;
+        }
+        int[] arr=new int[n];
+        int i=0;
+        while(head!=null){
+           arr[i++]=head.val;
+           head=head.next; 
+        }
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            if(arr[left]!=arr[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
