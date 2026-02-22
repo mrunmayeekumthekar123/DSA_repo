2b9135f71454afe782eca07cf3d802d6495962e8
mrunmayeekumthekar123 <mrunmayeekumthekar@gmail.com>
2026-02-22 17:09:35 +0530

    Update basiccalculator.java

--- string/basiccalculator.java
+++ string/basiccalculator.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+class Solution {
+    char[] arr;
+    int index=0;
+    public int calculate(String s) {
+        arr=s.toCharArray();
+        return dfs();
+    }
+    private int dfs(){
+        int sum=0;
+        int operator=1;
+        while(index<arr.length){
+            if(arr[index]==')'){
+                break;
+            }else if(arr[index]=='('){
+                index++;
+                sum+=operator*dfs();
+            }else if(arr[index]=='-'){
+                operator=-1;
+            }else if(arr[index]=='+'){
+                operator=1;
+            }else if(Character.isDigit(arr[index])){
+                StringBuilder buildnum=new StringBuilder();
+                while(index<arr.length && Character.isDigit(arr[index])){
+                    buildnum.append(arr[index++]);
+                }
+                index--;
+                long num = Long.parseLong(buildnum.toString());
+                sum += num * operator;
+            }
+            index++;
+        }
+        return sum;
+    }
+}
